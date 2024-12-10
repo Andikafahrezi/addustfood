a2d5e729dde2f5376b7cf39e16a7ec623c43c865
addust <andikafahrezi5@gmail.com>
2024-12-10 12:51:53 +0700

    docs: Add documents for easy configuration

--- DATABASE FILE/db_addustfood.sql
+++ DATABASE FILE/db_addustfood.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2024 at 02:00 PM
+-- Generation Time: Dec 10, 2024 at 06:22 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_rmkita`
+-- Database: `db_addustfood`
 --
 
 -- --------------------------------------------------------
@@ -63,10 +63,10 @@ CREATE TABLE `dishesh` (
 --
 
 INSERT INTO `dishesh` (`d_id`, `r_id`, `name`, `about`, `price`, `img`) VALUES
-(1, 3, 'Kuesoes', 'enak', 50000, 'suscoklat.jpeg'),
-(2, 1, 'Kue Soes ', 'Kue Soes coklat yang enak dan bergizi', 50000, 'suscoklat.jpeg'),
+(1, 0, 'soescoklat', 'enak mantape', 50000, 'suscoklat.jpeg'),
+(2, 0, 'Kue Soes', 'Kue Soes coklat yang enak dan bergizie', 50000, 'suscoklat.jpeg'),
 (3, 3, 'Kue Nastar', 'enak', 50000, 'nastar.jpeg'),
-(4, 2, 'Bolu Kukus', 'Bolu kukus Gula merah yang menjadikanya enak mantap', 5000, 'bolukukus.jpeg'),
+(4, 0, 'Mie Ayam', 'Wenake', 25000, 'bolukukus.jpeg'),
 (5, 2, 'Kue Lapis', 'Kue Lapis yang enak dan mantap', 5000, 'kuelapis.jpg'),
 (6, 2, 'Kue Dadar Gulung', 'Kue Dadar Gulung yang enak dan mantap', 5000, 'kuedadargulung.jpg'),
 (7, 2, 'Kue Cucur', 'Kue Cucur yang enak dan mantap', 5000, 'Kuecucur.jpeg'),
@@ -77,7 +77,7 @@ INSERT INTO `dishesh` (`d_id`, `r_id`, `name`, `about`, `price`, `img`) VALUES
 (12, 4, 'Rawon Jawa Timur', 'Rawon Khas Jawa Timur yang enak dan mantap', 15000, 'RawonJawaTimur.jpeg'),
 (13, 4, 'Ayam Betutu', 'Ayam Betutu yang enak dan mantap', 15000, 'AyamBetutu.jpeg'),
 (14, 4, 'Sate Ayam', 'Sate Ayam yang enak dan mantap', 15000, 'sateayam.jpg'),
-(15, 4, 'Gurame Bakar', 'Gurame Bakar yang enak dan mantap', 15000, 'Guramebakar.jpeg'),
+(15, 8, 'Gurame Bakar', 'Gurame Bakar yang enak dan mantap', 15000, 'Guramebakar.jpeg'),
 (16, 4, 'Nasi Goreng', 'Nasi Goreng yang enak dan mantap', 15000, 'nasigor.jpg'),
 (17, 4, 'Steak', 'Steak daging sapi yang enak dan mantap', 15000, 'stkkk.jpg'),
 (18, 3, 'Onde-Onde', 'Onde-Onde yang sangat enak dan mantap', 5000, 'Onde-Onde.jpeg'),
@@ -86,10 +86,11 @@ INSERT INTO `dishesh` (`d_id`, `r_id`, `name`, `about`, `price`, `img`) VALUES
 (21, 3, 'Keripik Tempe', 'Keripik Tempe yang gurih dan enak', 5000, 'keripiktempe.jpg'),
 (22, 3, 'Kentang Goreng', 'Kentang Goreng yang gurih dan enak', 15000, 'ffries.jpg'),
 (23, 3, 'Bakso', 'Bakso yang gurih dan enak', 15000, 'bakso.jpg'),
-(24, 1, 'Putri Salju', 'Putri Salju yang enak dan bergizi', 50000, 'putrisalju.jpeg'),
+(24, 8, 'Putri Salju', 'stock habis', 50000, 'putrisalju.jpeg'),
 (25, 1, 'Nastar', 'Nastar yang enak dan bergizi', 50000, 'Nastar.jpeg'),
 (26, 1, 'Lidah Kucing', 'Lidah Kucing yang enak dan bergizi', 50000, 'lidahkucing.jpeg'),
-(27, 1, 'Kastaangle', 'Kastaangle yang enak dan bergizi', 50000, 'kastangle.jpeg');
+(27, 1, 'Kastaangle', 'Kastaangle yang enak dan bergizi', 50000, 'kastangle.jpeg'),
+(33, 1, 'a', 'a', 50, 'ffries1.jpg');
 
 -- --------------------------------------------------------
 
@@ -110,7 +111,7 @@ CREATE TABLE `display` (
 --
 
 INSERT INTO `display` (`di_id`, `name`, `price`, `about`, `image`) VALUES
-(1, 'soescoklat', 50000, 'enak mantap', 'suscoklat.jpeg'),
+(1, 'soescoklata', 50000, 'enak mantap', 'suscoklat.jpeg'),
 (2, 'Kentang Goreng', 25000, 'Wenak', 'ffries.jpg'),
 (3, 'Nasi Goreng', 25000, 'Wenak', 'nasigor.jpg'),
 (4, 'Mie Ayam', 25000, 'Wenak', 'Mieayam.jpeg');
@@ -141,9 +142,10 @@ CREATE TABLE `restaurants` (
 
 INSERT INTO `restaurants` (`r_id`, `c_id`, `name`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `img`) VALUES
 (1, 5, 'Kue Kering', 'kelompok6@gmail.com', '0876876876876', 'www.addustcorp.site', '8am', '7pm', 'mon-sat', 'Menyediakan Kue Kering untuk kita semua', 'kering.jpeg'),
-(2, 8, 'Kue Basah', 'kelompok2@gmail.com', '0876876876876', 'www.tkfajarnusantara.site', '8am', '8pm', 'mon-sat', 'Menyediakan Kue Basah untuk kita semua', 'basah.jpg'),
-(3, 9, 'Makanan Ringan', 'kelompok2@gmail.com', '0876876876876', 'www.tkfajarnusantara.site', '8am', '8pm', 'mon-sat', 'Menyediakan Makanan Ringan untuk kita semua', 'displayringan.jpg'),
-(4, 9, 'Makanan Berat', 'Kelompok2@gmail.com', '0876876876876', 'www.tkfajarnusantara.site', '8am', '8pm', 'mon-sat', 'Menyediakan Makanan Berat untuk kita semua', 'berat.png');
+(2, 8, 'Kue Basah', 'kelompok6@gmail.com', '0876876876876', 'www.tkfajarnusantara.site', '8am', '8pm', 'mon-sat', 'Menyediakan Kue Basah untuk kita semua', 'basah.jpg'),
+(3, 9, 'Makanan Ringan', 'kelompok6@gmail.com', '0876876876876', 'www.tkfajarnusantara.site', '8am', '8pm', 'mon-sat', 'Menyediakan Makanan Ringan untuk kita semua', 'displayringan.jpg'),
+(4, 9, 'Makanan Berat', 'Kelompok6@gmail.com', '0876876876876', 'www.tkfajarnusantara.site', '8am', '8pm', 'mon-sat', 'Menyediakan Makanan Berat untuk kita semua', 'berat.png'),
+(8, 11, 'Stock Tidak Tersedia', 'habis@gmail.com', '080000000', 'habis.com', '11am', '8pm', '24hr-x7', 'Makanan Sedang Tidak Tersedia / Telah habis. Silahkan Cek kembali nanti', 'habis.jpg');
 
 -- --------------------------------------------------------
 
@@ -164,7 +166,8 @@ INSERT INTO `res_category` (`c_id`, `c_name`) VALUES
 (5, 'kering'),
 (8, 'Basah'),
 (9, 'Ringan'),
-(10, 'Berat');
+(10, 'Berat'),
+(11, 'Habis');
 
 -- --------------------------------------------------------
 
@@ -188,15 +191,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`u_id`, `username`, `f_name`, `l_name`, `email`, `phone`, `password`, `address`) VALUES
-(24, 'Ari', 'bekati', 'ari', 'ria@gmail.com', '086012225470', '$2y$10$pl0kXL/4376t7les3MR5EeKVVZrGxTDzhNjCmqyzkjheeQs5zEJwG', 'jakarta utara'),
-(28, 'wiwin', 'P', 'wiwin', 'wiwin@gmail.com', '08254580002', '$2y$10$S87MjBD29LIXmtegDjxa7uiGNKqUiLoMiecJ9vIRxUU9fCtdnfavO', 'rawamangun'),
-(33, 'heri', 'Henry', 'hendri', 'henrdy@gmail.com', '085402225000', '$2y$10$yKsf5a6TcTBHEflcfJDKnOu6Hfsw1QNok58uvrj7YWemzM2yMLNZC', 'jatinegara'),
-(34, 'juki', 'juki', 'juki', 'juki@gmail.com', '08758744260', '$2y$10$FvSDmYTKWLh9CWuhRUHsfuWqALfXwzR7jIC00y0ZSvTihaFCSnWeG', 'bekasi'),
-(35, 'dila69', 'adhila', 'putri', 'adhil@gmail.com', '0822449879', '$2y$10$h7VfLBNtTSQbIZb89U0.8eBtIwdrOiDZdYRgqNom41kuQp70oi1ny', 'jember'),
-(36, 'adhita1', 'adhita', 'putri', 'adhita@gmail.com', '0824567898', '$2y$10$nhz2PKMPJuN/I.Bvz778x.lYrz8z6En2vo2DkzIHyXi1UUtj..j9O', 'lumajang'),
 (37, 'tgegs', 'qwrw', 'wew', 'a@gmail.com', '0822449879', '$2y$10$N4N5nGJAzpysqePbDC3K4OWYAyklXqWMtcgQJPer3DcTOqXXcjBVa', 'eewffw'),
-(38, 'akram', 'akram', 'akram', 'akram@gmail.com', '0880918292', '$2y$10$otSuFbputYVR.Ter.dY1m.zcNlCXeKMuc7Bcxxmoba9zFdx/ZbJPe', 'a'),
-(39, 'aaaaa', 'aaaa', 'aaaa', 'a@gmail.com', '0211100324', '$2y$10$48yvQ2vJgWcICXoWNuQawOYAsH0EVd8tQf.gfPZMydmCCRuhUdBIC', 'dqdwdqq');
+(39, 'aaaaa', 'aaaa', 'aaaa', 'a@gmail.com', '0211100324', '$2y$10$48yvQ2vJgWcICXoWNuQawOYAsH0EVd8tQf.gfPZMydmCCRuhUdBIC', 'dqdwdqq'),
+(40, 'aaaaa', 'asep', 'wantep', 'asix@gmail.com', '0819153241', '$2y$10$2YR9mSU9TAu2IeW6ehWHYeL8eLvWyB/f6gpROFoC11s4ikwjZ830S', 'Jalan jalan'),
+(41, 'user1', 'user', 'user', 'user@gmail.com', '0825145623', '$2y$10$oK00F.otbrKt9y0xzXTMHefj/v7pQoAYXzIl3laMVWiG4.j.ZQILC', 'wqeqdq'),
+(42, 'user2', 'Basir', 'Jenggo', 'basirjenggo@gmail.com', '8135684252', '$2y$10$Tw5bBfbXnHY.4dmmSX5Y6.n5G.H92xc/iPPLugqQqv88oKGCechjW', 'Jalan Utama untuk mendapatkan nilai A. No 012');
 
 -- --------------------------------------------------------
 
@@ -214,30 +213,52 @@ CREATE TABLE `user_orders` (
   `status` varchar(255) DEFAULT NULL,
   `date` datetime NOT NULL DEFAULT current_timestamp(),
   `success-date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `r_id` int(11) NOT NULL
+  `r_id` int(11) NOT NULL,
+  `payment_mode` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user_orders`
 --
 
-INSERT INTO `user_orders` (`o_id`, `u_id`, `d_id`, `d_name`, `quantity`, `price`, `status`, `date`, `success-date`, `r_id`) VALUES
-(34, 36, 12, 'Mie Ayam', 1, 15000, 'closed', '2023-03-12 17:35:42', '2023-03-12 16:38:31', 2),
-(39, 38, 24, 'Nasi Goreng', 1, 40000, 'rejected', '2024-06-20 12:21:55', '2024-06-20 13:33:50', 2),
-(58, 39, 1, 'Kuesoes', 1, 50000, 'closed', '2024-10-05 09:12:21', '2024-10-05 07:13:16', 3),
-(59, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-05 09:19:23', '2024-10-05 07:19:36', 1),
-(60, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-05 12:07:50', '2024-10-05 10:16:20', 1),
-(61, 39, 3, 'Kue Nastar', 2, 100000, 'closed', '2024-10-05 12:15:59', '2024-10-05 10:16:15', 3),
-(62, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-05 12:15:59', '2024-10-05 10:16:10', 1),
-(63, 39, 5, 'Kue Lapis', 1, 5000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:36', 2),
-(64, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:32', 1),
-(65, 39, 22, 'Kentang Goreng', 1, 15000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:24', 3),
-(66, 39, 16, 'Nasi Goreng', 1, 15000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:21', 4),
-(67, 39, 2, 'Kue Soes ', 1, 50000, 'closed', '2024-10-06 17:34:04', '2024-10-06 15:34:20', 1),
-(68, 39, 27, 'Kastaangle', 1, 50000, 'closed', '2024-10-06 17:34:04', '2024-10-06 15:34:12', 1),
-(69, 39, 11, 'Ayam rica-rica', 1, 15000, 'closed', '2024-10-09 03:04:37', '2024-10-09 01:05:00', 4),
-(70, 39, 6, 'Kue Dadar Gulung', 1, 5000, 'closed', '2024-10-09 03:04:37', '2024-10-09 01:04:53', 2),
-(71, 39, 9, 'Gudeg Jogja', 2, 30000, 'closed', '2024-10-11 15:38:35', '2024-10-11 13:39:31', 4);
+INSERT INTO `user_orders` (`o_id`, `u_id`, `d_id`, `d_name`, `quantity`, `price`, `status`, `date`, `success-date`, `r_id`, `payment_mode`) VALUES
+(34, 36, 12, 'Mie Ayam', 1, 15000, 'closed', '2023-03-12 17:35:42', '2023-03-12 16:38:31', 2, 'Cash On Delivery'),
+(39, 38, 24, 'Nasi Goreng', 1, 40000, 'rejected', '2024-06-20 12:21:55', '2024-06-20 13:33:50', 2, 'Cash On Delivery'),
+(58, 39, 1, 'Kuesoes', 1, 50000, 'closed', '2024-10-05 09:12:21', '2024-10-05 07:13:16', 3, 'Cash On Delivery'),
+(59, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-05 09:19:23', '2024-10-05 07:19:36', 1, 'Cash On Delivery'),
+(60, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-05 12:07:50', '2024-10-05 10:16:20', 1, 'Cash On Delivery'),
+(61, 39, 3, 'Kue Nastar', 2, 100000, 'closed', '2024-10-05 12:15:59', '2024-10-05 10:16:15', 3, 'Cash On Delivery'),
+(62, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-05 12:15:59', '2024-10-05 10:16:10', 1, 'Cash On Delivery'),
+(63, 39, 5, 'Kue Lapis', 1, 5000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:36', 2, 'Cash On Delivery'),
+(64, 39, 2, 'bakso', 1, 50000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:32', 1, 'Cash On Delivery'),
+(65, 39, 22, 'Kentang Goreng', 1, 15000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:24', 3, 'Cash On Delivery'),
+(66, 39, 16, 'Nasi Goreng', 1, 15000, 'closed', '2024-10-06 17:19:14', '2024-10-06 15:19:21', 4, 'Cash On Delivery'),
+(67, 39, 2, 'Kue Soes ', 1, 50000, 'closed', '2024-10-06 17:34:04', '2024-10-06 15:34:20', 1, 'Cash On Delivery'),
+(68, 39, 27, 'Kastaangle', 1, 50000, 'closed', '2024-10-06 17:34:04', '2024-10-06 15:34:12', 1, 'Cash On Delivery'),
+(69, 39, 11, 'Ayam rica-rica', 1, 15000, 'closed', '2024-10-09 03:04:37', '2024-10-09 01:05:00', 4, 'Cash On Delivery'),
+(70, 39, 6, 'Kue Dadar Gulung', 1, 5000, 'closed', '2024-10-09 03:04:37', '2024-10-09 01:04:53', 2, 'Cash On Delivery'),
+(71, 39, 9, 'Gudeg Jogja', 2, 30000, 'closed', '2024-10-11 15:38:35', '2024-10-11 13:39:31', 4, 'Cash On Delivery'),
+(72, 39, 4, 'Bolu Kukus', 1, 5000, 'closed', '2024-11-23 07:41:29', '2024-11-23 06:49:34', 2, 'Cash On Delivery'),
+(73, 39, 6, 'Kue Dadar Gulung', 1, 5000, 'rejected', '2024-11-23 07:41:29', '2024-11-23 06:52:02', 2, 'Cash On Delivery'),
+(76, 41, 6, 'Kue Dadar Gulung', 1, 5000, 'closed', '2024-11-23 07:52:47', '2024-11-23 06:52:57', 2, 'Cash On Delivery'),
+(77, 41, 9, 'Gudeg Jogja', 1, 15000, 'closed', '2024-11-30 10:49:46', '2024-12-01 07:45:25', 4, 'Cash On Delivery'),
+(78, 41, 6, 'Kue Dadar Gulung', 1, 5000, 'closed', '2024-12-01 09:21:24', '2024-12-01 08:21:40', 2, 'Cash On Delivery'),
+(110, 41, 9, 'Gudeg Jogja', 1, 15000, 'closed', '2024-12-02 13:52:03', '2024-12-02 12:52:24', 4, 'Credit Card'),
+(112, 41, 5, 'Kue Lapis', 1, 5000, 'closed', '2024-12-02 14:10:28', '2024-12-02 13:13:25', 2, 'Credit Card'),
+(113, 41, 18, 'Onde-Onde', 1, 5000, 'closed', '2024-12-02 14:13:08', '2024-12-02 13:13:22', 3, 'Credit Card'),
+(115, 41, 26, 'Lidah Kucing', 1, 50000, 'closed', '2024-12-02 14:14:35', '2024-12-02 13:14:43', 1, 'E-Wallet'),
+(118, 41, 24, 'Putri Salju', 1, 50000, 'closed', '2024-12-02 14:16:28', '2024-12-02 13:16:49', 1, 'E-Wallet'),
+(124, 41, 10, 'Rendang Sapi', 1, 15000, 'closed', '2024-12-02 14:34:43', '2024-12-02 13:34:51', 4, 'Credit Card'),
+(125, 41, 9, 'Gudeg Jogja', 1, 15000, 'closed', '2024-12-02 14:40:04', '2024-12-02 13:40:10', 4, 'E-Wallet'),
+(126, 41, 8, 'Martabak', 1, 5000, 'closed', '2024-12-02 14:46:29', '2024-12-02 13:46:36', 2, 'E-Wallet'),
+(127, 41, 9, 'Gudeg Jogja', 1, 15000, 'closed', '2024-12-09 15:19:21', '2024-12-09 14:19:36', 4, NULL),
+(128, 41, 15, 'Gurame Bakar', 1, 15000, 'closed', '2024-12-09 15:19:21', '2024-12-09 14:19:31', 4, NULL),
+(129, 41, 7, 'Kue Cucur', 1, 5000, 'closed', '2024-12-09 15:20:12', '2024-12-09 14:20:20', 2, 'Cash'),
+(130, 41, 9, 'Gudeg Jogja', 1, 15000, 'closed', '2024-12-09 15:20:46', '2024-12-09 14:21:00', 4, NULL),
+(131, 41, 7, 'Kue Cucur', 1, 5000, 'closed', '2024-12-09 15:20:46', '2024-12-09 14:20:56', 2, 'E-Wallet'),
+(132, 42, 5, 'Kue Lapis', 1, 5000, 'closed', '2024-12-10 05:11:56', '2024-12-10 04:12:06', 2, 'E-Wallet'),
+(133, 42, 11, 'Ayam rica-rica', 1, 15000, 'closed', '2024-12-10 05:12:45', '2024-12-10 04:40:58', 4, 'Credit Card'),
+(136, 42, 26, 'Lidah Kucing', 1, 50000, 'closed', '2024-12-10 06:15:16', '2024-12-10 05:15:28', 1, 'Credit Card');
 
 --
 -- Indexes for dumped tables
@@ -299,31 +320,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `dishesh`
 --
 ALTER TABLE `dishesh`
-  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `restaurants`
 --
 ALTER TABLE `restaurants`
-  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `res_category`
 --
 ALTER TABLE `res_category`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `user_orders`
 --
 ALTER TABLE `user_orders`
-  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ readme.md
@@ -0,0 +1,82 @@
+# README: E-Commerce Website Project
+
+## Project Description
+This project is a web-based e-commerce application built using **PHP** with the **CodeIgniter** framework. The application is designed to facilitate users in making online purchases and provide an easy management experience for admins through a dedicated dashboard.
+
+### Key Features
+#### User
+- **Order Products**: Users can browse various product categories, add items to the cart, and complete purchases.
+- **Payment**: The system supports multiple payment methods to ensure smooth transactions.
+- **Transaction Reports**: Users can view their purchase history.
+
+#### Admin Dashboard
+- **Manage Menu**: Admins can add, edit, or delete menu items.
+- **Manage Users**: A feature to manage user data, including access rights.
+- **Manage Popular Menus**: Admins can mark and manage frequently purchased menus.
+- **Manage Food Types**: Organize food items by type (e.g., snacks, beverages, etc.).
+- **Manage Categories**: Add, edit, and delete product categories.
+
+### Technologies Used
+- **Backend**: PHP with CodeIgniter framework
+- **Database**: MySQL
+- **Frontend**: HTML, CSS, JavaScript
+- **Additional Libraries**:
+  - Bootstrap for styling
+  - jQuery for interactivity
+  - DataTables for dynamic tables and reports
+
+### Installation
+1. **Clone the Repository**
+   ```bash
+   git clone https://github.com/Andikafahrezi/addustfood.git
+   ```
+2. **Database Configuration**
+   - Create a new database in MySQL.
+   - Import the `addustfood.sql` file located in the `database/` folder into the database.
+   - Update the database configuration in the `application/config/database.php` file.
+
+3. **Configure Base URL**
+   - Modify the `base_url` in `application/config/config.php` to match your application's URL.
+
+4. **Run the Application**
+   - Ensure your local web server (XAMPP, WAMP, etc.) is running.
+   - Access the application via your browser using the appropriate URL.
+
+### How to Use
+#### Users
+1. Register or log in to start using the application.
+2. Add products to your shopping cart.
+3. Proceed to the checkout page to complete your payment.
+
+#### Admin
+1. Log in using admin credentials.
+2. Use the dashboard to manage menus, categories, users, and reports.
+
+### Directory Structure
+- `application/`: Contains CodeIgniter files (controllers, models, views).
+- `assets/`: Contains static files like CSS, JS, and images.
+- `database/`: Contains SQL files for the database structure.
+- `uploads/`: Stores user-uploaded files (e.g., product images).
+
+### Contribution
+1. Fork this repository.
+2. Create a new branch for your feature or fix.
+   ```bash
+   git checkout -b feature-name
+   ```
+3. Make changes and commit them.
+   ```bash
+   git commit -m "Add new feature"
+   ```
+4. Push the branch to your repository.
+   ```bash
+   git push origin feature-name
+   ```
+5. Create a Pull Request to the main repository.
+
+### License
+This project is licensed under the MIT License. See the `LICENSE` file for more details.
+
+---
+
+If you have any questions, feel free to contact [andikafahrezi5@gmail.com](mailto:andikafahrezi5@gmail.com).
